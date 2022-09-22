8e9d56bd04a4e6117abc4a86c661a0db7e6b85d4
sonyaaat <sofiatkach2004@gmail.com>
2022-09-22 15:36:28 +0300

    Task1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn=document.querySelector("[data-start]")
+const stopBtn=document.querySelector("[data-stop]")
 
+let timeoutId=null;
+
+startBtn.addEventListener("click", onStartClick)
+stopBtn.addEventListener("click",onStopClick)
+
+function onStartClick()
+{
+    changeColor();
+    timeoutId = setInterval(changeColor, 1000);
+    startBtn.setAttribute("disabled",true)
+    stopBtn.removeAttribute('disabled')
+}
+
+function onStopClick()
+{
+    clearInterval(timeoutId)
+    startBtn.removeAttribute("disabled")
+    stopBtn.setAttribute("disabled",true)
+}
+
+function changeColor()
+{
+document.body.style.backgroundColor=getRandomHexColor();
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
